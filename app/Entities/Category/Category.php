62330bd4922827f98dd6c2711d75f53271c8890e
mechanizzed <dsibr@hotmail.com>
2017-10-27 00:13:28 -0200

    Added entities Category and Product

---
+++ app/Entities/Category/Category.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppService\Entities\Category;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+	protected $table "agrupo01";
+}
---
+++ app/Entities/Product/Product.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppService\Entities\Product;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+	protected $table = "produtos01";
+}
